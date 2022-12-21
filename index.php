f4deafc4889663e719b356bf6c41a14c1c1a94d2
ManganiBiagio <biagiomangani@live.it>
2022-12-21 17:40:05 +0100

    new file function + required

--- index.php
+++ index.php
@@ -1,30 +1,8 @@
 <?php
+require "./functions.php";
 
 $lengthPsw = $_GET['lengthPsw'];
 $psw = generatePsw($lengthPsw);
-
-
-
-
-function generatePsw($lengthPsw)
-{
-    $charMaiuscList = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    $charMinList = strtolower($charMaiuscList);
-    $numberList = "0123456789";
-    $simbolList = "@ç%£!?§#*";
-
-    // ora genero una unica stringa dove poter estrarre i miei elementi 
-    $allChar = $charMaiuscList . $charMinList . $numberList . $simbolList;
-    $toReturn = "";
-    for ($i = 0; $i < $lengthPsw; $i++) {
-        $toReturn .= $allChar[rand(0, strlen($allChar) - 1)];
-    }
-    return $toReturn;
-}
-
-
-
-
 
 ?>
 
