b0c7631af395bbaff87d663fbaf42fd34d904f8d
ManganiBiagio <biagiomangani@live.it>
2022-12-21 17:37:36 +0100

    fybction psw generator

--- index.php
+++ index.php
@@ -1,7 +1,26 @@
 <?php
 
-$lengthPsw=$_GET['lengthPsw'];
-var_dump($lengthPsw);
+$lengthPsw = $_GET['lengthPsw'];
+$psw = generatePsw($lengthPsw);
+
+
+
+
+function generatePsw($lengthPsw)
+{
+    $charMaiuscList = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    $charMinList = strtolower($charMaiuscList);
+    $numberList = "0123456789";
+    $simbolList = "@ç%£!?§#*";
+
+    // ora genero una unica stringa dove poter estrarre i miei elementi 
+    $allChar = $charMaiuscList . $charMinList . $numberList . $simbolList;
+    $toReturn = "";
+    for ($i = 0; $i < $lengthPsw; $i++) {
+        $toReturn .= $allChar[rand(0, strlen($allChar) - 1)];
+    }
+    return $toReturn;
+}
 
 
 
@@ -11,6 +30,7 @@ var_dump($lengthPsw);
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -19,24 +39,25 @@ var_dump($lengthPsw);
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
     <div class="container">
         <div class="text-center mb-3">
-            <h1>Password Generator</h1>
+            <h4>Password Generator</h4>
         </div>
-        
+
         <div class="row justify-content-center">
             <div class="col-12">
-            <form method="GET" class="row gx-3 gy-2 align-items-center justify-content-center">
+                <form method="GET" class="row gx-3 gy-2 align-items-center justify-content-center">
                     <div class="col-sm-3 d-flex align-items-center">
                         <label class="me-2">Lunghezza password:</label>
-                        <input type="number" class="form-control" name="lengthPsw" id="specificSizeInputName" placeholder="..." >
+                        <input type="number" class="form-control" name="lengthPsw" id="specificSizeInputName" placeholder="...">
                     </div>
 
 
                     <div class="col-auto">
                         <div class="form-check">
-                            <input class="form-check-input " name="isParcheggio" type="checkbox" id="autoSizingCheck2"  >
+                            <input class="form-check-input " name="isParcheggio" type="checkbox" id="autoSizingCheck2">
                             <label class="form-check-label" for="autoSizingCheck2">
                                 Parcheggio
                             </label>
@@ -46,9 +67,16 @@ var_dump($lengthPsw);
                         <button class="btn btn-primary">Cerca</button>
                     </div>
                 </form>
+
             </div>
+            <?php if(!empty($psw)){ ?>
+            <div class="col-12 text-center mt-4" >
+                Risultato=<?php echo $psw ?>
+            </div>
+            <?php }?>
         </div>
     </div>
-    
+
 </body>
+
 </html>
